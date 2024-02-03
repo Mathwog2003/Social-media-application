a454863fd4d4800cea2e63809b3f1cd60f9150a5
mathwog2003 <gowthamselvaraj037@gmail.com>
2024-02-03 12:57:34 +0530

    added spring configuration class and tested with basic authentication

--- Final-year-project/src/main/java/com/social/demo/controller/UserController.java
+++ Final-year-project/src/main/java/com/social/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.social.demo.controller;
 
 import com.social.demo.entity.User;
+import com.social.demo.repositary.UserRepository;
 import com.social.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
@@ -15,18 +16,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/users")
     public User createUser(@RequestBody User user)
     {
         return userService.registerUser(user);
     }
 
-   @PutMapping("/users/{id}")
+   @PutMapping("/api/users/{id}")
    public User updateUser(@RequestBody User user, @PathVariable Integer id) throws Exception {
        return userService.updateUser(user,id);
    }
 
-   @GetMapping("/users/{id}")
+   @GetMapping("/api/users")
+   public List<User> getUsers()
+   {
+       return userRepository.findAll();
+   }
+   @GetMapping("/api/users/{id}")
    public Optional<User> findUserById(@PathVariable Integer id) throws Exception {
        return userService.findUserById(id);
    }
@@ -41,12 +50,12 @@ public class UserController {
        userService.deleteuser(id);
    }
 
-   @PutMapping("/users/follow/{id1}/{id2}")
+   @PutMapping("/api/users/follow/{id1}/{id2}")
     public User followUserHandler(@PathVariable Integer id1,@PathVariable Integer id2) throws Exception {
        return userService.followUser(id1,id2);
    }
 
-   @GetMapping("/users/search")
+   @GetMapping("/api/users/search")
     public List<User> searchUser(@RequestParam("query") String query)
    {
        return  userService.searchUser(query);
